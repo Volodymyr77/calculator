e155d1d1b49eaf1318a88b90f1e4c2d30aa64874
Volodymyr77 <gagarinvov@gmail.com>
2021-03-08 18:18:25 +0200

    calc third

---
+++ src/simpleCalculator.java
@@ -0,0 +1,56 @@
+//import javax.swing.*;
+//import java.awt.GridLayout;
+//import java.awt.FlowLayout;
+//
+//
+//public class simpleCalculator {
+//    public static void main(String[] args) {
+//
+//        // create panel
+//
+//        JPanel windowContent = new JPanel();
+//
+//        //manager visibility of panel
+//
+//        FlowLayout fl = new FlowLayout();
+////        windowContent.setLayout(fl);
+//
+//
+//
+//
+//        // create memory components
+//
+//        JLabel label1 = new JLabel("Number 1:");
+//        JTextField field1 = new JTextField(10);
+//        JLabel label2 = new JLabel("Number 2:");
+//        JTextField field2 = new JTextField(10);
+//        JLabel label3 = new JLabel("Sum:");
+//        JTextField result = new JTextField(10);
+//        JButton go = new JButton("Add");
+//
+//        // add components on panel
+//
+//        windowContent.add(label1);
+//        windowContent.add(field1);
+//        windowContent.add(label2);
+//        windowContent.add(field2);
+//        windowContent.add(label3);
+//        windowContent.add(result);
+//        windowContent.add(go);
+//
+//        //create frame and let panel for it
+//
+//        JFrame frame = new JFrame("my First Calculator");
+//        frame.setContentPane(windowContent);
+//
+//        // set size and make files visible
+//
+//        frame.setSize(400,100);
+//        frame.setVisible(true);
+//
+//
+//
+//
+//    }
+//
+//}
---
+++ src/sipmleCalculatorGrid.java
@@ -0,0 +1,58 @@
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.GridLayout;
+//
+//public class sipmleCalculatorGrid {
+//
+//    public static void main(String[] args) {
+//
+//        // create panel
+//
+//        JPanel windowContent = new JPanel();
+//
+//        //manager visibility of panel
+//
+//        GridLayout gl = new GridLayout(4,2);
+//
+//        windowContent.setLayout(gl);
+//
+//
+//
+//
+//        // create memory components
+//
+//        JLabel label1 = new JLabel("Number 1:");
+//        JTextField field1 = new JTextField(10);
+//        JLabel label2 = new JLabel("Number 2:");
+//        JTextField field2 = new JTextField(10);
+//        JLabel label3 = new JLabel("Sum:");
+//        JTextField result = new JTextField(10);
+//        JButton go = new JButton("Add");
+//
+//        // add components on panel
+//
+//        windowContent.add(label1);
+//        windowContent.add(field1);
+//        windowContent.add(label2);
+//        windowContent.add(field2);
+//        windowContent.add(label3);
+//        windowContent.add(result);
+//        windowContent.add(go);
+//
+//        //create frame and let panel for it
+//
+//        JFrame frame = new JFrame("my First Calculator");
+//        frame.setContentPane(windowContent);
+//
+//        // set size and make files visible
+//
+//        frame.setSize(400,100);
+//        frame.setVisible(true);
+//
+//
+//
+//
+//    }
+//
+//}
+//
